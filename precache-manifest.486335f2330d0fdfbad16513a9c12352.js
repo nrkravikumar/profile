c7504db7b353d668f38bd1b84ea9d8ac88cc9763
NRK <rravikumar34@gmail.com>
2020-04-30 19:26:23 +0530

    Updates

---
+++ precache-manifest.486335f2330d0fdfbad16513a9c12352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb368d5bca9766e2fc9e4a5bcc0e1950",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "5370f3e92bda01e6a7ff",
+    "url": "/profile/static/css/main.f43fec10.chunk.css"
+  },
+  {
+    "revision": "889813513e8f3ae3ccb8",
+    "url": "/profile/static/js/2.29743bbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile/static/js/2.29743bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5370f3e92bda01e6a7ff",
+    "url": "/profile/static/js/main.228b4c97.chunk.js"
+  },
+  {
+    "revision": "026b7bb7607ddb643782",
+    "url": "/profile/static/js/runtime-main.1d60ddc2.js"
+  }
+]);
